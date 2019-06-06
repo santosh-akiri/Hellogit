95e3a22cbe80e52f7abe148ac0b4d6a31476c257
santosh akiri <santosh.akiri@yahoo.com>
2019-06-06 08:16:46 +0530

    Added everything

---
+++ AKIRI_PRACTISE/src/com/Numbr.java
@@ -0,0 +1,20 @@
+package com;
+
+public class Numbr {
+	
+	Numbr(){
+		System.out.println("0");
+	}
+
+	public  static void main(String args[]){
+		Numbr num= new Numbr();
+		System.out.println(num.Number());
+
+	}
+	
+
+private  int Number(){
+	
+	return 1;
+}
+}
