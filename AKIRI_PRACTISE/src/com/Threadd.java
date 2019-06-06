142395d1dc56152c48008296cf6c367112493198
santosh akiri <santosh.akiri@yahoo.com>
2019-06-06 08:15:17 +0530

    Changes done in Threadd.java

---
+++ AKIRI_PRACTISE/src/com/Threadd.java
@@ -0,0 +1,22 @@
+package com;
+
+public class Threadd implements Runnable {
+	public  void run(){ 
+		Thread t1=new Thread();
+		System.out.println("thread is running...and Gitbash added");  
+		for(int i=0;i<10;i++){
+			System.out.println(i);
+			
+		}
+		}  
+	
+	
+		  
+		public static void main(String args[]){  
+			Threadd m1=new Threadd();  
+		Thread t1 =new Thread(m1);  
+		Thread t2 =new Thread(m1);
+		t1.start();
+		t2.start();
+		 } 
+}
